b23d8ced241181b4809d6904db238885acb2e7e4
Mayank Mhatre <121260524+mayank477@users.noreply.github.com>
2026-08-24 20:20:02 +0530

    Add JavaScript Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/217. Contains Duplicate/Solution.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    //two pointer i and j 
+
+    //create map 
+    // add elemets with freq 
+    // iterate in maps, then if freq >1 , return true 
+    //else false 
+
+
+    let m = new Map();
+    for (let i=0;i<nums.length;i++){
+        if(m.has(nums[i])){
+            let prevfreq = m.get(nums[i]);
+            m.set(nums[i],prevfreq+1);
+        }
+        else{
+            m.set(nums[i],1);
+        }
+    }
+
+    for (let [key,value] of m){
+        if(value>1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    } 
+};
